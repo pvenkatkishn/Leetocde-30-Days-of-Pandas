ccf00372df7813a5bd82718740049c30a72a51a6
Prasanthraaja Venkatkishna <vprasanthraaja@gmail.com>
2025-02-21 16:30:13 -0800

    Create 1683. Invalid Tweets.py

---
+++ String Methods/1683. Invalid Tweets.py
@@ -0,0 +1,24 @@
+Table: Tweets
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of characters on an American Keyboard, and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Return the result table in any order.
+
+
+Solution:
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    invalid_tweets = tweets[tweets["content"].str.len() > 15]
+    return invalid_tweets[["tweet_id"]]
