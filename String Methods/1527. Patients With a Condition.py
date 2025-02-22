1c38220c9ea9d4b658f8f8b4b8c02c677def4cfd
Prasanthraaja Venkatkishna <vprasanthraaja@gmail.com>
2025-02-21 16:32:51 -0800

    Create 1527. Patients With a Condition.py

---
+++ String Methods/1527. Patients With a Condition.py
@@ -0,0 +1,25 @@
+Table: Patients
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key (column with unique values) for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+ 
+
+Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+Return the result table in any order.
+
+
+Solution:
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+       df = patients[patients['conditions'].str.contains(r"\bDIAB1", regex=True)]
+       return df
