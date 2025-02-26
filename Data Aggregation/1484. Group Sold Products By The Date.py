9a38be586f9e8b719cf106dd94a93330a2178df9
Prasanthraaja Venkatkishna <vprasanthraaja@gmail.com>
2025-02-25 22:35:58 -0800

    Create 1484. Group Sold Products By The Date.py

---
+++ Data Aggregation/1484. Group Sold Products By The Date.py
@@ -0,0 +1,26 @@
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+ 
+
+Write a solution to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+
+
+
+Solution:
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    df = activities.groupby("sell_date")["product"]..reset_index()
+    return df
